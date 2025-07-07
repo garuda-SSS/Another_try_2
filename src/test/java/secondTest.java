93a5769c255eb05a02e587ef189d46cf847436a1
garuda-SSS <ysipin1997@gmail.com>
2025-07-07 16:09:30 +0300

    Копировал штуки из старого репозитория)

---
+++ src/test/java/secondTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class secondTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"admin", "user", "guest"})
+    void testLogin(String role) {
+        System.out.println(role);
+        assertNotNull(role);
+        assertTrue(true);
+        assertFalse(false);
+        System.out.println(Thread.currentThread().getName() + " — " + "testLogin");
+    }
+}
